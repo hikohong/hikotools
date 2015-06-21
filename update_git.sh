2065064b42aa18a561c4aaa5621b1b5d6e1a941f
hikohong <hikohong@gmail.com>
2015-06-21 23:43:25 +0800

    update git branches folder by folder - must be in one directory level for now

---
+++ update_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#declare -a symb_flies=(`find . -type l`)
+declare -a symb_flies=(`ls`)
+#echo "debug: $symb_flies"
+
+#searching symbolic links
+#echo "debug: count=${#symb_flies[@]}"
+for ((i=0; i<${#symb_flies[@]}; i++)); do
+	if [ -d "${symb_flies[$i]}" ]; then
+		cd "${symb_flies[$i]}"
+		echo "current dir: `pwd`"
+		if [ -d ".git" ]; then
+			git pull --rebase
+			echo "hit!"
+		fi
+		cd ../
+	fi
+done
+
+
