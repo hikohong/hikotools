d5eb3ffb032961a94be2104d789dd346569020c1
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2016-09-27 20:52:43 +0800

    add the vim parsing code and uvc dedicated

---
+++ parse_uvc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PARSE_BK=~/parsing_bk
+PARSE_PATH=$1
+if [ -z "$PARSE_PATH" ]; then
+	#print usage
+    echo "please input the path for parsing"
+    echo "parse_uvc.sh <PATH>"
+	exit 1
+fi
+
+if  [ -d $PARSE_BK ]; then
+    rm -rf $PARSE_BK
+fi
+mkdir $PARSE_BK
+mv ./unifi-video-firmware/openwrt-gen* $PARSE_BK
+
+./parse_vim.sh $PARSE_PATH --rdb
+
+mv $PARSE_BK/openwrt-gen* ./unifi-video-firmware/
+
+
---
+++ parse_vim.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+CS_FILE=cscope.file
+PARSE_BK=~/parsing_bk
+PARSE_PATH=$1
+PARSE_OPT="--CLEAN_DB"
+
+if [ -z "$PARSE_PATH" ]; then
+    #print usage
+    echo "please input the path for parsing"
+    echo "parse_vim.sh <PATH> [OPT]"
+    echo "OPT: --cdb(default) --rdb"
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    case $2 in
+    #clean PARSE_DB
+    --cdb)
+    PARSE_OPT=--CLEAN_DB
+    ;;
+    #remain PARSE_DB
+    --rdb)
+    PARSE_OPT=--REMAIN_DB
+    ;;
+    esac
+fi
+
+################
+# main procedure
+################
+rm $PARSE_PATH/tags
+rm $PARSE_PATH/cscope*
+
+echo "backup data"
+if [ $PARSE_OPT=="CLEAN_DB" ]; then
+    if  [ -d $PARSE_BK ]; then
+        rm -rf $PARSE_BK
+    fi
+fi
+mkdir $PARSE_BK
+mv $PARSE_PATH/*.vim $PARSE_BK
+
+#parsing process
+echo "start parsing"
+find $PARSE_PATH -name "*.aidl" \
+    -o -name "*.cc" \
+    -o -name "*.h" \
+    -o -name "*.c" \
+    -o -name "*.cpp" \
+    -o -name "*.java" \
+    -o -name "*.mk" \
+    > $CS_FILE
+ctags -R --exclude=.svn --exclude=.git --exclude=.vim --exclude=out
+cscope -bkq -i $CS_FILE
+#cscope -Rbk -i $CS_FILE
+
+
+echo "restore backup data"
+mv $PARSE_BK/*.vim $PARSE_PATH
+if [ $PARSE_OPT=="CLEAN_DB" ]; then
+    rm -rf $PARSE_BK
+fi
+
