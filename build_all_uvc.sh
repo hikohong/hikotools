58d78572f146b8fda07a690a4d66d3dd7398f42b
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2016-10-30 17:25:19 +0800

    add middleware build

--- build_all_uvc.sh
+++ build_all_uvc.sh
@@ -13,21 +13,37 @@ function show_usage()
     echo "   ./build_all_uvc.sh -b --dep ./"
     echo "-----------------------------------------------------------------------------------"
     echo "[BUILD_OPTION] the building option"
-    echo " -b --build (default) exec building process"
+    echo " -b --build build fw bin files"
+    echo " --bm --buid_mid build middleware libs"
+    echo " --ba --build_all build both middleware and fw bin"
     echo " --nb exec no building process"
     echo "[DEPLOY_OPTION] optional setting"
-    echo "  --nodep(default) --dep"
+    echo "  --nd --nodep(default) --dep -d"
     echo "[CODEBASE_ROOT] The codebase root"
     echo "  ./(default) (current location)"
     echo "==================================================================================="
 }
 
+function make_middleware()
+{
+    cd $GEN2_MID_PATH
+    make package/ubnt-middleware/compile V=99
+    cd $GEN3L_MID_PATH
+    make package/ubnt-middleware/compile V=99
+    cd $GEN3M_MID_PATH
+    make package/ubnt-middleware/compile V=99
+    cd $CODEBASE_ROOT
+}
 
-CODEBASE_ROOT="."
-GEN2_BIN_PATH=$CODEBASE_ROOT/openwrt-gen2-dbg/artifacts
-GEN3L_BIN_PATH=$CODEBASE_ROOT/openwrt-gen3l-dbg/artifacts
-GEN3M_BIN_PATH=$CODEBASE_ROOT/openwrt-gen3m-dbg/artifacts
-BUILD_PROCESS=true
+
+CODEBASE_ROOT=`pwd`
+GEN2_MID_PATH=$CODEBASE_ROOT/openwrt-gen2-dbg
+GEN3L_MID_PATH=$CODEBASE_ROOT/openwrt-gen3l-dbg
+GEN3M_MID_PATH=$CODEBASE_ROOT/openwrt-gen3m-dbg
+GEN2_BIN_PATH=$GEN2_MID_PATH/artifacts
+GEN3L_BIN_PATH=$GEN3L_MID_PATH/artifacts
+GEN3M_BIN_PATH=$GEN3M_MID_PATH/artifacts
+BUILD_PROCESS=fw
 DEPLOY_PROCESS=false
 
 
@@ -44,8 +60,8 @@ GEN3L_IPADDRS=(
 GEN3M_IPADDRS=(
 "10.2.1.134"
 "10.2.1.183"
-"10.2.1.92"
-"10.2.1.196"
+#"10.2.1.92"
+#"10.2.1.196"
 )
 
 ##############
@@ -57,8 +73,14 @@ if [ -n "$1" ]; then
         --nb)
         BUILD_PROCESS=false
         ;;
-        -b)
-        BUILD_PROCESS=true
+        -b|--build)
+        BUILD_PROCESS=fw
+        ;;
+        --bm|build_mid)
+        BUILD_PROCESS=middleware
+        ;;
+        --ba|--buid_all)
+        BUILD_PROCESS=fw_middleware
         ;;
         #this should be in latest case of the switch
         -h|*)
@@ -73,10 +95,10 @@ fi
 
 if [ -n "$2" ]; then
     case $2 in
-        --dep)
+        -d|--dep)
         DEPLOY_PROCESS=true
         ;;
-        --nodep)
+        --nd|--nodep)
         DEPLOY_PROCESS=false
         ;;
         *)
@@ -88,34 +110,48 @@ fi
 
 if [ -n "$3" ]; then
     CODEBASE_ROOT=$3
-    GEN2_BIN_PATH=$CODEBASE_ROOT/openwrt-gen2-dbg/artifacts
-    GEN3L_BIN_PATH=$CODEBASE_ROOT/openwrt-gen3l-dbg/artifacts
-    GEN3M_BIN_PATH=$CODEBASE_ROOT/openwrt-gen3m-dbg/artifacts
+    GEN2_MID_PATH=$CODEBASE_ROOT/openwrt-gen2-dbg
+    GEN3L_MID_PATH=$CODEBASE_ROOT/openwrt-gen3l-dbg
+    GEN3M_MID_PATH=$CODEBASE_ROOT/openwrt-gen3m-dbg
+    GEN2_BIN_PATH=$GEN2_MID_PATH/artifacts
+    GEN3L_BIN_PATH=$GEN3L_MID_PATH/artifacts
+    GEN3M_BIN_PATH=$GEN3M_MID_PATH/artifacts
 fi
 
 #building gen2 gen3l and gen3m
-if [ "$BUILD_PROCESS" == "true" ]; then
-    make gen2-dbg/fw
-    make gen3l-dbg/fw
-    make gen3m-dbg/fw
-fi
+case $BUILD_PROCESS in
+    fw)
+        make gen2-dbg/fw
+        make gen3l-dbg/fw
+        make gen3m-dbg/fw
+    ;;
+    middleware)
+        make_middleware
+    ;;
+    fw_middleware)
+        make_middleware
+        make gen2-dbg/fw
+        make gen3l-dbg/fw
+        make gen3m-dbg/fw
+    ;;
+esac
 
 if [ "$DEPLOY_PROCESS" == "true" ]; then
     for GEN2_DEV_IP in "${GEN2_IPADDRS[@]}"; do
         echo "scp into GEN2 $GEN2_DEV_IP"
-        find $GEN2_BIN_PATH/ -name *.bin -exec sshpass -p ubnt scp {} ubnt@$GEN2_DEV_IP:/tmp/fwupdate.bin \;
+        find $GEN2_BIN_PATH/ -name UVC*.bin -exec sshpass -p ubnt scp {} ubnt@$GEN2_DEV_IP:/tmp/fwupdate.bin \;
         sshpass -p ubnt ssh ubnt@$GEN2_DEV_IP 'fwupdate -m'
     done
 
     for GEN3L_DEV_IP in "${GEN3L_IPADDRS[@]}"; do
         echo "scp into GEN3L $GEN3L_DEV_IP"
-        find $GEN3L_BIN_PATH/ -name *.bin -exec sshpass -p ubnt scp {} ubnt@$GEN3L_DEV_IP:/tmp/fwupdate.bin \;
+        find $GEN3L_BIN_PATH/ -name UVC*.bin -exec sshpass -p ubnt scp {} ubnt@$GEN3L_DEV_IP:/tmp/fwupdate.bin \;
         sshpass -p ubnt ssh ubnt@$GEN3L_DEV_IP 'fwupdate -m'
     done
 
     for GEN3M_DEV_IP in "${GEN3M_IPADDRS[@]}"; do
         echo "scp into GEN3M $GEN3M_DEV_IP"
-        find $GEN3M_BIN_PATH/ -name *.bin -exec sshpass -p ubnt scp {} ubnt@$GEN3M_DEV_IP:/tmp/fwupdate.bin \;
+        find $GEN3M_BIN_PATH/ -name UVC*.bin -exec sshpass -p ubnt scp {} ubnt@$GEN3M_DEV_IP:/tmp/fwupdate.bin \;
         sshpass -p ubnt ssh ubnt@$GEN3M_DEV_IP 'fwupdate -m'
     done
 fi
