6eb54be72c7f86a8b6f898e0c5545c2675a1319f
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2017-02-15 19:35:36 +0800

    add new tool

---
+++ push_ubnt_ispserver_so.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+IP_ADDR=10.2.129.168
+if [ $1 ]; then
+    $IP_ADDR=$1
+else
+    show_usage
+    exit 1
+fi
+
+sshpass -p ubnt ssh ubnt@$IP_ADDR 'mount -o rw,remount /'
+sshpass -p ubnt scp openwrt-gen3m-dbg/build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
+sshpass -p ubnt ssh ubnt@$IP_ADDR reboot
