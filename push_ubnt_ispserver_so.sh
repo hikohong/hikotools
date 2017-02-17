97379f73706577dbd67d14ad2d896726dba83d2c
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2017-02-17 19:50:33 +0800

    add new ips and refine

--- push_ubnt_ispserver_so.sh
+++ push_ubnt_ispserver_so.sh
@@ -2,11 +2,15 @@
 IP_ADDR=10.2.129.168
 if [ $1 ]; then
     $IP_ADDR=$1
-else
-    show_usage
-    exit 1
+#else
+#    show_usage
+#    exit 1
 fi
 
+echo "building package"
+make package/ubnt-middleware/compile V=99
+echo "updateing to $IP_ADDR"
 sshpass -p ubnt ssh ubnt@$IP_ADDR 'mount -o rw,remount /'
-sshpass -p ubnt scp openwrt-gen3m-dbg/build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
+#sshpass -p ubnt scp openwrt-gen3m-dbg/build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
+sshpass -p ubnt scp build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
 sshpass -p ubnt ssh ubnt@$IP_ADDR reboot
