1e16feaa69ce6947c232f8d3edd14fdfae0b65ee
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2017-02-20 17:51:10 +0800

    typo

--- push_ubnt_ispserver_so.sh
+++ push_ubnt_ispserver_so.sh
@@ -9,7 +9,7 @@ fi
 
 echo "building package"
 make package/ubnt-middleware/compile V=99
-echo "updateing to $IP_ADDR"
+echo "updating to $IP_ADDR"
 sshpass -p ubnt ssh ubnt@$IP_ADDR 'mount -o rw,remount /'
 #sshpass -p ubnt scp openwrt-gen3m-dbg/build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
 sshpass -p ubnt scp build_dir/target-arm-openwrt-linux-uclibcgnueabi/ubnt-middleware/ipkg-ambarella/ubnt-middleware/usr/lib/libubnt_ispserver_*  ubnt@$IP_ADDR:/lib/
