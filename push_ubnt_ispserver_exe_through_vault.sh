9da4c6d88639ce7c22f149e93067ac0b7e21d387
hiko.hong@ubnt.com <hiko.hong@ubnt.com>
2017-05-08 20:13:16 +0800

    update new version and support vault input

---
+++ push_ubnt_ispserver_exe_through_vault.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+IP_ADDR_VAULT=10.2.128.51
+IP_ADDR_CAM=172.30.0.165
+if [ $1 ]; then
+    $IP_ADDR_VAULT=$1
+    $IP_ADDR_CAM=$2
+#else
+#    show_usage
+#    exit 1
+fi
+
+echo "building ubnt_ispserver exe"
+#cd "/home/hikohong/Android/unifi-video-fw_sync/unifi-video-firmware/openwrt-gen3m-dbg/"
+#make package/ubnt-middleware/compile V=99
+cd "/home/hikohong/Android/unifi-video-fw_sync/unifi-video-firmware/openwrt-gen3m-dbg/package/ubnt-middleware/unifi-video-fw-middleware"
+./builders/cmake/utils/compile -p gen3m
+
+echo "updating to $IP_ADDR_VAULT"
+#sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'mount -o rw,remount /'
+
+echo "scp ubnt_ispserver into vault"
+sshpass -p ubnt scp /home/hikohong/Android/unifi-video-fw_sync/unifi-video-firmware/packages-other/ubnt-middleware/unifi-video-fw-middleware/builders/cmake/output/gen3m/MinSizeRel/STATIC/rootfs/usr/bin/ubnt_ispserver  ubnt@$IP_ADDR_VAULT:/tmp/
+
+echo "scp ubnt_ispserver from vault to cam"
+sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt scp /tmp/ubnt_ispserver ubnt@172.30.0.165:/tmp/'
+
+#sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt scp /tmp/ubnt_ispserver ubnt@$IP_ADDR_CAM:/tmp/'
+echo "install ubnt_ispserver in camera"
+sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt ssh ubnt@172.30.0.165 "sed -i \"s/null::respawn:\/bin\/ubnt_ispserver/null::respawn:\/tmp\/ubnt_ispserver/\" /etc/inittab"'
+#sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt ssh ubnt@172.30.0.165 "kill -1 1 && killall ubnt_ispserver && /tmp/ubnt_ispserver &"'
+sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt ssh ubnt@172.30.0.165 "kill -1 1 && killall ubnt_ispserver"'
+#echo "reboot camera"
+#sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'sshpass -p ubnt ssh ubnt@172.30.0.165 reboot'
+
+#echo "getting camera log"
+#sshpass -p ubnt ssh ubnt@$IP_ADDR_VAULT 'rm /tmp/out && sshpass -p ubnt ssh ubnt@172.30.0.165 logread > /tmp/out'
+#sshpass -p ubnt scp ubnt@$IP_ADDR_VAULT:/tmp/out .
