c4768ca8183e397eb20fb9b4cbf4ffebb53cd89c
hikohong <hikohong@gmail.com>
2017-12-09 17:14:11 +0000

    type

--- system_tools/change_user_acc.sh
+++ system_tools/change_user_acc.sh
@@ -12,7 +12,7 @@ sudo usermod -c "Vulcan DB" $CURR_ACC_NAME
 sudo mv /home/$CURR_ACC_NAME /home/$NEW_ACC_NAME
 sudo usermod -d /home/$NEW_ACC_NAME $CURR_ACC_NAME
 
-#change user groud
+#change user group
 sudo groupadd $USER_GROUP
 sudo usermod -g $USER_GROUP $CURR_ACC_NAME
 
