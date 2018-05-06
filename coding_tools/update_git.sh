14a50a9e067d1af6e0efc2d19a6f4399a793209a
hikohong <hikohong@gmail.com>
2018-05-06 13:00:51 +0800

    redine update_git workind

--- coding_tools/update_git.sh
+++ coding_tools/update_git.sh
@@ -4,17 +4,17 @@ declare -a symb_flies=(`ls`)
 #echo "debug: $symb_flies"
 
 #searching symbolic links
-#echo "debug: count=${#symb_flies[@]}"
+echo "folder count : ${#symb_flies[@]}"
 for ((i=0; i<${#symb_flies[@]}; i++)); do
-	if [ -d "${symb_flies[$i]}" ]; then
-		cd "${symb_flies[$i]}"
-		echo "current dir: `pwd`"
-		if [ -d ".git" ]; then
-			git pull --rebase
-			echo "hit!"
-		fi
-		cd ../
-	fi
+    if [ -d "${symb_flies[$i]}" ]; then
+        cd "${symb_flies[$i]}"
+        echo "current dir: `pwd`"
+        if [ -d ".git" ]; then
+            git pull --rebase
+            echo "hit!"
+        fi
+        cd ../
+    fi
 done
 
 
